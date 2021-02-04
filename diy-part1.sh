eb456c90c8a153edcd8720fc627514cb6f5d126e
wangjiali888 <73116101+wangjiali888@users.noreply.github.com>
2021-02-04 09:56:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,4 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 git clone https://github.com/linkease/ddnsto.git package/ddnsto
+git clone https://github.com/linkease/ddnsto.git
